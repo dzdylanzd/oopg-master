5efdecaba2762c7fd8d473ef955d823bfb4868d5
dylan <dylanzweers@gmail.com>
2019-03-19 16:40:55 +0100

    Update TestWorld.java

--- src/main/java/testGame/TestWorld.java
+++ src/main/java/testGame/TestWorld.java
@@ -1,10 +1,41 @@
 package testGame;
 
-public class TestWorld {
+import nl.han.ica.oopg.engine.GameEngine;
+import nl.han.ica.oopg.view.View;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	// deze 3 classes moet je geimporteerd hebben om het te laten werken.
+	
+	import nl.han.ica.oopg.objects.TextObject;
+
+
+	public class TestWorld extends GameEngine {
+
+		public static void main(String[] args) {
+			TestWorld hw = new TestWorld();
+
+					// deze methode start de game.
+			hw.runSketch();
+		}
+
+		@Override
+		public void setupGame() {
+			int worldWidth = 500;
+			int worldHeight = 500;
+			
+			TextObject to = new TextObject("Hello World!", 40);
+			to.setForeColor(255, 255, 255, 255);
+			addGameObject(to, 100, 100);
+			
+			View view = new View(worldWidth, worldHeight);
+
+			setView(view);
+			size(worldWidth, worldHeight);
+		}
+
+		@Override
+		public void update() {
+			// Dit doet nog niets
+			
+		}
 
 	}
-
-}
