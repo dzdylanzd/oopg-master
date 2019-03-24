d46c8b3a29bad5c5106f104e817d954f05875a0a
dylan <dylanzweers@gmail.com>
2019-03-24 13:03:03 +0100

    Create Koning.java

---
+++ src/main/java/schaakSpel/Koning.java
@@ -0,0 +1,22 @@
+package schaakSpel;
+
+import nl.han.ica.oopg.objects.Sprite;
+import nl.han.ica.oopg.objects.SpriteObject;
+
+public class Koning extends SpriteObject {
+
+	public Koning(schaakSpel world,String text) {
+		super(new Sprite("src/main/java/nl/han/ica/oopd/schaakSpel/media/" + text + "King.png"));
+		
+		   
+	}
+
+	
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
