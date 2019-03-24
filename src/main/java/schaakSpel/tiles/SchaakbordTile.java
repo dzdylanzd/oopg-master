35b8c3880cb5ff746de6a69b6e41ce8dea618984
dylan <dylanzweers@gmail.com>
2019-03-24 13:03:18 +0100

    Create SchaakbordTile.java

---
+++ src/main/java/schaakSpel/tiles/SchaakbordTile.java
@@ -0,0 +1,16 @@
+package schaakSpel.tiles;
+
+
+import nl.han.ica.oopg.objects.Sprite;
+import nl.han.ica.oopg.tile.Tile;
+
+
+public class SchaakbordTile extends Tile {
+    /**
+     * @param sprite The image which will be drawn whenever the draw method of the Tile is called.
+     */
+    public SchaakbordTile(Sprite sprite) {
+        super(sprite);
+    }
+    
+}
