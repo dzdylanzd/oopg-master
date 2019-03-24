b0dca2caa348c7e19afc326c2b6c984d7e701ba5
dylan <dylanzweers@gmail.com>
2019-03-24 13:57:34 +0100

    Create Paard.java

---
+++ src/main/java/schaakSpel/Paard.java
@@ -0,0 +1,45 @@
+package schaakSpel;
+
+import java.util.List;
+
+import nl.han.ica.oopg.collision.CollidedTile;
+import nl.han.ica.oopg.collision.CollisionSide;
+import nl.han.ica.oopg.exceptions.TileNotFoundException;
+import nl.han.ica.oopg.objects.Sprite;
+import processing.core.PVector;
+import schaakSpel.tiles.SchaakbordTile;
+
+public class Paard extends schaakstuk {
+
+	public Paard(schaakSpel world,String text) {
+		super(new Sprite("src/main/java/schaakSpel/media/" + text + "Knight.png"));
+		
+		this.world = world;
+	}
+
+	@Override
+	public void tileCollisionOccurred(List<CollidedTile> collidedTiles) {
+		 PVector vector;
+
+		for (CollidedTile ct : collidedTiles) {
+	        if (ct.getTile() instanceof SchaakbordTile) {
+	              if (CollisionSide.INSIDE.equals(ct.getCollisionSide())) {
+	             try {
+	                 vector = world.getTileMap().getTilePixelLocation(ct.getTile());
+	              setX((vector.x+getWidth()/3));
+	              setY((vector.y+getHeight()/3));
+	             } catch (TileNotFoundException e) {
+	                 e.printStackTrace();
+	             }
+	        }
+	}
+	}
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
