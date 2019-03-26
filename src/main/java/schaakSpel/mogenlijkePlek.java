3e5c7be9e46671ee01509a088252d0315b6c4258
watzegjij <48723994+watzegjij@users.noreply.github.com>
2019-03-26 23:30:18 +0100

    Update mogenlijkePlek.java

--- src/main/java/schaakSpel/mogenlijkePlek.java
+++ src/main/java/schaakSpel/mogenlijkePlek.java
@@ -1,16 +1,7 @@
 package schaakSpel;
 
-import java.util.List;
-
-import nl.han.ica.oopg.collision.CollidedTile;
-import nl.han.ica.oopg.collision.CollisionSide;
-import nl.han.ica.oopg.collision.ICollidableWithTiles;
-import nl.han.ica.oopg.exceptions.TileNotFoundException;
 import nl.han.ica.oopg.objects.Sprite;
 import nl.han.ica.oopg.objects.SpriteObject;
-import nl.han.ica.oopg.userinput.IMouseInput;
-import processing.core.PVector;
-import schaakSpel.tiles.SchaakbordTile;
 
 public class mogenlijkePlek extends SpriteObject  {
 
@@ -18,13 +9,11 @@ public class mogenlijkePlek extends SpriteObject  {
 	
 	public mogenlijkePlek(schaakSpel world) {
 		super(new Sprite("src/main/java/schaakSpel/media/selecteerVakje.png"));
-		// TODO Auto-generated constructor stub
 		this.world = world;
 	}
 
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
 		
 	}
 	
