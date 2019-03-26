ba7e1ecb0997f0ca98df10f2167f4d98acc667d2
watzegjij <48723994+watzegjij@users.noreply.github.com>
2019-03-26 23:30:23 +0100

    Update Pion.java

--- src/main/java/schaakSpel/Pion.java
+++ src/main/java/schaakSpel/Pion.java
@@ -1,46 +1,20 @@
 package schaakSpel;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import nl.han.ica.oopg.collision.CollidedTile;
-import nl.han.ica.oopg.collision.CollisionSide;
-import nl.han.ica.oopg.exceptions.TileNotFoundException;
 import nl.han.ica.oopg.objects.Sprite;
-import processing.core.PVector;
-import schaakSpel.tiles.SchaakbordTile;
+import nl.han.ica.oopg.sound.Sound;
 
 public class Pion extends schaakstuk {
 	
 	private String ZW;
-	private boolean verplaatst = false;
-	
-	
+	private boolean verplaatst = false;	
 
-	public Pion(schaakSpel world,String text) {
-		super(new Sprite("src/main/java/schaakSpel/media/" + text + "Pawn.png"));
+	public Pion(schaakSpel world,String text, Sound pickupSound, Sound putdownSound) {
+		super(new Sprite("src/main/java/schaakSpel/media/" + text + "Pawn.png"),pickupSound,putdownSound);
 		this.ZW = text;
 		this.world = world;
 	}
 	
-	@Override
-	public void tileCollisionOccurred(List<CollidedTile> collidedTiles) {
-		 PVector vector;
 
-		for (CollidedTile ct : collidedTiles) {
-	        if (ct.getTile() instanceof SchaakbordTile) {
-	              if (CollisionSide.INSIDE.equals(ct.getCollisionSide())) {
-	             try {
-	                 vector = world.getTileMap().getTilePixelLocation(ct.getTile());
-	              setX((vector.x+getWidth()/3));
-	              setY((vector.y+getHeight()/3));
-	             } catch (TileNotFoundException e) {
-	                 e.printStackTrace();
-	             }
-	        }
-	}
-	}
-	}
 
 	
 	@Override
