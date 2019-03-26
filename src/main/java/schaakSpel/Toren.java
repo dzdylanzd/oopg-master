2638d183eb009dafd9acc972ac50584f5e2c09d1
dylan <dylanzweers@gmail.com>
2019-03-26 15:12:53 +0100

    Update Toren.java

--- src/main/java/schaakSpel/Toren.java
+++ src/main/java/schaakSpel/Toren.java
@@ -1,5 +1,6 @@
 package schaakSpel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import nl.han.ica.oopg.collision.CollidedTile;
@@ -10,6 +11,8 @@ import processing.core.PVector;
 import schaakSpel.tiles.SchaakbordTile;
 
 public class Toren extends schaakstuk {
+	
+
 
 	public Toren(schaakSpel world,String text) {
 		super(new Sprite("src/main/java/schaakSpel/media/" + text + "Rook.png"));
@@ -36,10 +39,24 @@ public class Toren extends schaakstuk {
 	}
 	}
 
+	
+	
 	@Override
-	public void update() {
-		// TODO Auto-generated method stub
+	protected void maakPlekken() {
+		float x = getX()-15;
+		float y = getY()-15;
+		float tileSize = 80;
+		float X[] = {-1,-2,-3,-4,-5,-6,-7,1,2,3,4,5,6,7,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+		float Y[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,-2,-3,-4,-5,-6,-7,1,2,3,4,5,6,7};
 		
+		for(int i=0; i < X.length; i++) {
+			if(x+tileSize*X[i] < 800-tileSize && x+tileSize*X[i] > tileSize && y+tileSize*Y[i] < 800-tileSize && y+tileSize*Y[i] > tileSize  ) {
+		mogenlijkePlek plek = new mogenlijkePlek(world);
+		plekken.add(plek);	
+		world.addGameObject(plek,x+tileSize*X[i],y+tileSize*Y[i]);
+			}
+		}
+
 	}
 
 }
