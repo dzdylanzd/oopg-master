7d92771e36c0d48a8e5d15b40a8acdc30cdc8648
dylan <dylanzweers@gmail.com>
2019-03-26 15:12:36 +0100

    Update Koningin.java

--- src/main/java/schaakSpel/Koningin.java
+++ src/main/java/schaakSpel/Koningin.java
@@ -1,5 +1,6 @@
 package schaakSpel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import nl.han.ica.oopg.collision.CollidedTile;
@@ -10,6 +11,8 @@ import processing.core.PVector;
 import schaakSpel.tiles.SchaakbordTile;
 
 public class Koningin extends schaakstuk {
+	
+	
 
 	public Koningin(schaakSpel world,String text) {
 		super(new Sprite("src/main/java/schaakSpel/media/" + text + "Queen.png"));
@@ -36,10 +39,23 @@ public class Koningin extends schaakstuk {
 	}
 	}
 
+	
 	@Override
-	public void update() {
-		// TODO Auto-generated method stub
+	protected void maakPlekken() {
+		float x = getX()-15;
+		float y = getY()-15;
+		float tileSize = 80;
+		float X[] = {-1,1,0,0};
+		float Y[] = {0,0,-1,1};
 		
+		for(int i=0; i < X.length; i++) {
+			if(x+tileSize*X[i] < 800-tileSize && x+tileSize*X[i] > tileSize && y+tileSize*Y[i] < 800-tileSize && y+tileSize*Y[i] > tileSize  ) {
+		mogenlijkePlek plek = new mogenlijkePlek(world);
+		plekken.add(plek);	
+		world.addGameObject(plek,x+tileSize*X[i],y+tileSize*Y[i]);
+			}
+		}
+
 	}
 
 }
